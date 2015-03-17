5578721d67626c2bc5d0a270919734860a0718a3
Joao Paulo <joao.paulo@smartbox.com>
2015-03-17 10:20:44 +0000

    Add widget para arquivo de categoria

--- category.php
+++ category.php
@@ -13,8 +13,8 @@
 get_header(); ?>
 
 	<section class="content cat">
-		<div class="row-fluid">
-			<div class="span8">			
+		<div class="row-fluid span12">
+			<div class="span8">
 				<h2><?php echo single_cat_title( '', false ); ?></h2>
 			<?php
 				while ( have_posts() ) : the_post();
@@ -27,15 +27,17 @@ get_header(); ?>
 					</article>
 			<?php			
 				endwhile; 
-				//the_bootstrap_content_nav();
 			?>
 			<div class="nav-previous">&nbsp;<?php previous_posts_link( '<< Página Anterior' ); ?></div>
 			<div class="nav-next">&nbsp;<?php next_posts_link( 'Próxima Página >>' ); ?></div>
 			
 			</div>
-			<?php get_sidebar(); ?>
+            <div class="span4">
+            <?php if (is_active_sidebar('content-right-column-1')) : ?>
+                <?php dynamic_sidebar('content-right-column-1'); ?>
+            <?php endif; ?>
+            </div>
 		</div>
 	</section>
-
 
 <?php get_footer(); ?>
