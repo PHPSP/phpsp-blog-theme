d40efa685b3e929d2214500ebac86fbd5f9c018e
Joao Paulo <jp.joao@gmail.com>
2015-03-18 09:01:10 +0000

    Refactor do arquivo por categoria

--- category.php
+++ category.php
@@ -15,16 +15,20 @@ get_header(); ?>
 	<section class="content cat">
 		<div class="row-fluid span12">
 			<div class="span8">
-				<h2><?php echo single_cat_title( '', false ); ?></h2>
+				<h1 class="blue-block"><strong>Categoria: </strong><?php echo single_cat_title('', false ); ?></h1>
 			<?php
 				while ( have_posts() ) : the_post();
 			?>
-					<article itemtype="http://schema.org/Article" itemscope>					
-						<h3><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h3>						
-						<time itemprop="dateCreated" datetime="<?php the_time('Y-m-d\TH:i'); ?>"><?php echo ucfirst(strtolower(get_the_time('l, j \d\e F, Y'))); ?></time>
-						<div class="author">By <span itemprop="author"><?php the_author(); ?> </span></div>					
-						<div class="excerpt"><?php the_excerpt(); ?></div>
-					</article>
+                <article itemtype="http://schema.org/Article" itemscope>
+                    <h2><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+                    <div class="author">
+                        <p>Por <span itemprop="author"><?php the_author_posts_link(); ?> </span>
+                            em <time itemprop="dateCreated" datetime="<?php the_time('Y-m-d\TH:i'); ?>"><?php echo strtolower(get_the_time('j \d\e F \d\e Y')); ?></time></span></p>
+                    </div>
+                    <div class="excerpt"><?php the_excerpt(); ?></div>
+                    <?php the_tags( '<p class="tags">Tags: ', ', ', '</p>'); ?>
+                    <div class="ver-mais"><a href="<?php the_permalink(); ?>" rel="bookmark">Ver mais...</a></div>
+                </article>
 			<?php			
 				endwhile; 
 			?>
