201a5650801d5b62152ead55234034d9e37cb76c
Joao Paulo <jp.joao@gmail.com>
2015-03-18 10:14:35 +0000

    Mostra a categoria quando nao listando arquivos de categoria Mostra a tag quando nao listando arquivos de categoria

--- archive-post.php
+++ archive-post.php
@@ -12,9 +12,15 @@
         <h2><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
         <div class="author">
             <p>Por <span itemprop="author"><?php the_author_posts_link(); ?> </span>
-                em <time itemprop="dateCreated" datetime="<?php the_time('Y-m-d\TH:i'); ?>"><?php echo strtolower(get_the_time('j \d\e F \d\e Y')); ?></time></span></p>
+                em <time itemprop="dateCreated" datetime="<?php the_time('Y-m-d\TH:i'); ?>"><?php echo strtolower(get_the_time('j \d\e F \d\e Y')); ?></time></span>
+                <?php if (!is_category()) : ?>
+                em <span itemprop="category"><?php the_category(','); ?></span>
+                <?php endif; ?>
+            </p>
         </div>
         <div class="excerpt"><?php the_excerpt(); ?></div>
+        <?php if(!is_tag()) : ?>
         <?php the_tags( '<p class="tags">Tags: ', ', ', '</p>'); ?>
+        <?php endif; ?>
         <div class="ver-mais"><a href="<?php the_permalink(); ?>" rel="bookmark">Ver mais...</a></div>
     </article>
