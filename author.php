59f81d745daddee42f1024391e1d4ca870e4f4e6
Joao Paulo <jp.joao@gmail.com>
2015-03-18 09:56:39 +0000

    Refactor do arquivo por autor

--- author.php
+++ author.php
@@ -13,8 +13,8 @@
 
 get_header(); ?>
 
-	<section id="primary" class="site-content">
-		<div id="content" role="main"class="row-fluid span12">
+	<section id="primary" class="content author-page archive">
+		<div class="row-fluid span12">
             <div class="span8">
 		<?php if ( have_posts() ) : ?>
 			<?php
@@ -28,24 +28,19 @@ get_header(); ?>
                     </div><!-- .author-avatar -->
 					<p><?php the_author_meta( 'description' ); ?></p>
 				</div><!-- .author-description	-->
+                <?php if(get_the_author_meta('url')): ?>
+                <div class="author-card">
+                    <p class="author-link">Site: <a href="<?php echo get_the_author_meta('url');?>"><?php echo get_the_author_meta('url'); ?></a></p>
+                </div>
+                <?php endif; ?>
 			</div><!-- .author-info -->
-            <h2 class="blue-block"><strong>Posts</strong> do autor</h2>
 			<?php endif; ?>
             <h2 class="blue-block"><strong>Posts</strong> de <?php the_author(); ?></h2>
 
-            <?php
-            /* Start the Loop */
-            while ( have_posts() ) : the_post();
-                ?>
-                <article itemtype="http://schema.org/Article" itemscope>
-                    <h3><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h3>
-                    <time itemprop="dateCreated" datetime="<?php the_time('Y-m-d\TH:i'); ?>"><?php echo ucfirst(strtolower(get_the_time('l, j \d\e F, Y'))); ?></time>
-                    <div class="author">By <span itemprop="author"><?php the_author(); ?> </span></div>
-                    <div class="excerpt"><?php the_excerpt(); ?></div>
-                </article>
-            <?php
-            endwhile;
-            ?>
+            <?php while ( have_posts() ) : the_post(); ?>
+                <?php get_template_part( 'archive', 'post' ); ?>
+            <?php endwhile; ?>
+            
             <div class="nav-previous">&nbsp;<?php previous_posts_link( '<< Página Anterior' ); ?></div>
             <div class="nav-next">&nbsp;<?php next_posts_link( 'Próxima Página >>' ); ?></div>
 
