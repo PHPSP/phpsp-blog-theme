1aaab6a3e8d1870bc4d0aaf2cb3e8eb0f6a6326a
Joao Paulo <jp.joao@gmail.com>
2015-03-20 17:00:26 +0000

    Remove os fallbakcs originais. Tudo agora eh widget.

--- index.php
+++ index.php
@@ -47,59 +47,15 @@ get_header(); ?>
 	<section class="content">
 		<div class="row-fluid">
             <div class="bl_eventos span4">
-            <?php if (!is_active_sidebar('home-column-1')) : ?>
-			    <h2 class="black-block">Próximos <strong>Eventos</strong></h2>
-			    <div class="row-fluid article-phpsp-home">
-					<div class="span12">
-						<p><a href="http://www.meetup.com/php-sp/">Logo mais... Aguardem.</a></p>
-					</div>
-				</div>
-            <?php else : ?>
                 <?php dynamic_sidebar('home-column-1'); ?>
-            <?php endif; ?>
             </div>
             <div class="bl_artigos span4">
-            <?php if (is_active_sidebar('home-column-2')) : ?>
                 <?php dynamic_sidebar('home-column-2'); ?>
-            <?php else : ?>
-                <h2 class="blue-block"><strong>Artigos</strong> da comunidade</h2>
-				<?php   wp_reset_query();
-					$args = array( 'category_name' => 'artigos', 'posts_per_page' => 5 );
-					$loop = new WP_Query( $args );
-					while ( $loop->have_posts() ): $loop->the_post();?>
-					<div class="row-fluid article-phpsp-home">
-					    <div class="span3">
-						<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'the-bootstrap_author_bio_avatar_size', 70 ) ); ?>
-						
-					    </div>
-					    <div class="span9">
-						<div class="art_title"><a href="<?php the_permalink(); ?>"><?php echo the_title();?></a></div>
-						<small class="art_por">por <?php the_author()?></small>
-					    </div>
-					</div>
-				<?php endwhile; ?>
-				<a class="todos_artigos" href="<?php echo get_bloginfo('url'); ?>/index.php/category/artigos/">Ver todos os artigos...</a>
-            <?php endif; ?>
             </div>
             <div class="bl_avisos span4">
-            <?php if (is_active_sidebar('home-column-3')) : ?>
                 <?php dynamic_sidebar('home-column-3'); ?>
-            <?php else : ?>
-                <h2 class="grey-block"><strong>Avisos</strong> da comunidade</h2>
-			    <?php
-				wp_reset_query();				
-				$loop = new WP_Query( array( 'category_name' => 'Avisos', 'posts_per_page' => 1 ) );
-				$loop->the_post(); ?>
-			    <div class="row-fluid article-phpsp-home">
-				<div class="span12">
-				    <div class="aviso"><?php the_excerpt()?></div>
-				    <strong><a href="<?php the_permalink(); ?>">Saiba mais</a></strong>
-				</div>
-			    </div>
-            <?php endif; ?>
             </div>
 		</div>
 	</section>
 
-<?php //get_sidebar(); ?>
 <?php get_footer(); ?>
